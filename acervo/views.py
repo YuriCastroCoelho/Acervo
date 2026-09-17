1858542ffe64045e93f636ed03585a3f17ca8408
Yuri <yuricastrocoelho90@gmail.com>
2026-09-16 21:14:10 -0300

    5

--- acervo/views.py
+++ acervo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Livro
+
 # Create your views here.
-def inicio(request):
-    return HttpResponse(
-        'Olá, acervo!'
+def lista_livros(request):
+    livros = Livro.objects.all()
+    return render(
+        request, 'acervo/lista.html',
+        {'livros': livros}
     )
